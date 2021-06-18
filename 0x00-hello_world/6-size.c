d9a5550a3d1ce75c424c8f5eaf55c9788a0b23d5
ackamir <ackamir@outlook.com>
2021-06-17 21:46:10 -0700

    Task 6 - Complying with betty's standards

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,13 +4,15 @@
  * main - This is a brief program that displays the size of data
  * Return: 0
  */
-int main() {
+int main(void)
+{
+
 	int a = 16;
-	printf("Size of a char: %d\n",sizeof(char)"byte(s)");
-	printf("Size of an int: %d\n",sizeof(int)"byte(s)");
-	printf("Size of a long int: %d\n",sizeof(long int)"byte(s)");
-	printf("Size of a long long int: %d\n",sizeof(long long int)"byte(s)");
-	printf("Size of float: %d\n",sizeof(float)"byte(s)");
-	return 0;
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of float: %d  byte(s)\n", sizeof(float));
+	return (0);
 
 }
