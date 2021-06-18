c9bfa83371458aa09db9fd84b9284eafe15c61a1
ackamir <ackamir@outlook.com>
2021-06-17 21:35:38 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This is a brief program that displays the size of data
+ * Return: 0
+ */
+int main() {
+	int a = 16;
+	printf("Size of a char: %d\n",sizeof(char));
+	printf("Size of an int: %d\n",sizeof(int));
+	printf("Size of a long int: %d\n",sizeof(long int));
+	printf("Size of a long long int: %d\n",sizeof(long long int));
+	printf("Size of float: %d\n",sizeof(float));
+	return 0;
+
+}
