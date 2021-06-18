b6ce17a2d85afd0787d9a421a1741bdeafa1f40f
ackamir <ackamir@outlook.com>
2021-06-17 21:38:38 -0700

    Task 6 - Added @byte (s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main() {
 	int a = 16;
-	printf("Size of a char: %d\n",sizeof(char));
-	printf("Size of an int: %d\n",sizeof(int));
-	printf("Size of a long int: %d\n",sizeof(long int));
-	printf("Size of a long long int: %d\n",sizeof(long long int));
-	printf("Size of float: %d\n",sizeof(float));
+	printf("Size of a char: %d\n",sizeof(char)"byte(s)");
+	printf("Size of an int: %d\n",sizeof(int)"byte(s)");
+	printf("Size of a long int: %d\n",sizeof(long int)"byte(s)");
+	printf("Size of a long long int: %d\n",sizeof(long long int)"byte(s)");
+	printf("Size of float: %d\n",sizeof(float)"byte(s)");
 	return 0;
 
 }
