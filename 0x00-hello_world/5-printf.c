d6e8278eab05b8ca324874a7bea76746db4e658e
ackamir <ackamir@outlook.com>
2021-06-17 21:23:33 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - A brief program that prints another phrase
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
