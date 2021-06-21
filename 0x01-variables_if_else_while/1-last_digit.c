34af7ed698caf7a2310ca6e613aa172ccc489c05
ackamir <ackamir@outlook.com>
2021-06-20 21:32:05 -0700

    Task 1 - Complying with Betty's standards

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,23 +15,23 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	LastDigit = n % 10;
 
-	printf("Last digit of %d is ",n);
+	printf("Last digit of %d is ", n);
 	printf("%d", LastDigit);
 
 	if ((LastDigit > 5))
 	{
 		printf(" and is greater than 5\n");
 		return (0);
-	} else {
+	}
 	if ((LastDigit == 0))
 	{
 		printf(" and is 0\n");
 		return (0);
-	} else {
+	}
 	if ((LastDigit < 6) && (LastDigit != 0))
 	{
 		printf(" and is less than 6 and not 0\n");
 		return (0);
-	}}}
+	}
 	return (0);
 }
