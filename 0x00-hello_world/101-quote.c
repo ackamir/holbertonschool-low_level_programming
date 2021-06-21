dce889a179bcc6134f65410e3cb82654aa2ba8c5
ackamir <ackamir@outlook.com>
2021-06-20 17:44:43 -0700

    Advanced Task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - A brief program that prints another phrase
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
