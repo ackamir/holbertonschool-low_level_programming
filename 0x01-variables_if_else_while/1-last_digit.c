66e6bc9cdfb553643c95e91b87e6a2de111a3d79
ackamir <ackamir@outlook.com>
2021-06-20 21:29:44 -0700

    Task 1 - Erased The

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	LastDigit = n % 10;
 
-	printf("The last digit of %d is ",n);
+	printf("Last digit of %d is ",n);
 	printf("%d", LastDigit);
 
 	if ((LastDigit > 5))
