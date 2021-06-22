d747ab1443c685b94617a9982c92cc9d7c5b8064
ackamir <ackamir@outlook.com>
2021-06-21 18:04:30 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digits used in hexadecimal
+ * Return: 0
+ */
+
+int main(void)
+{
+	char hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+
+	{
+		putchar(hex);
+	}
+
+	for (hex = 'a'; hex <= 'f'; hex++)
+
+	{
+		putchar(hex);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
